b59ec7a99ac33effa43bd560dd83d713802bba83
JiYoonKimjimmy <36873758+JiYoonKimjimmy@users.noreply.github.com>
2018-08-08 14:47:39 +0900

    Initial commit

---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="index">
+    <select v-model="selected">
+      <option v-for="option in options" v-bind:value="option.value">
+        {{ option.text }}
+      </option>
+    </select>
+    <span>선택함: {{ selected }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      selected: 'A',
+      options: [
+        {text:'one', value:'A'},
+        {text:'two', value:'B'},
+        {text:'three', value:'C'}
+      ]
+    }
+  }
+}
+</script>
